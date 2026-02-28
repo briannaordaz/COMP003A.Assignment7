9776a7770251cc7a58beea8c0f1c6f68b10c45f8
briannaordaz <briannaordaz5@gmail.com>
2026-02-27 23:18:59 -0800

    added a try-catch to prevent user from entering a non-numeric value

--- COMP003A.Assignment7/Program.cs
+++ COMP003A.Assignment7/Program.cs
@@ -16,70 +16,103 @@ class Program
         Console.Write("1. Enter Test Scores\n2. Display Scores\n3. Total Score\n4. Average Score\n5. Exit");
 
 
-        int number;
+        int number = 0;
 
         do
         {
-            Console.WriteLine("Enter a menu option (1-4): ");
-            number = int.Parse(Console.ReadLine());
+            try
+            {
 
-                
+                Console.WriteLine("\nEnter a menu option (1-5): ");
+                number = int.Parse(Console.ReadLine());
+
+
                 if (number < 1 || number > 5)
                 {
+
+
                     Console.WriteLine("Invalid option. Please try again.");
 
                 }
                 else if (number == 1)
                 {
+
+
                     Console.WriteLine("Enter Test Subject: ");
                     string subjectName = Console.ReadLine();
                     subject[index] = subjectName;
-                    
+
                     Console.WriteLine("Enter Test Score: ");
                     int subjectScore = int.Parse(Console.ReadLine());
                     scores[index] = subjectScore;
                     index++;
 
+
                 }
                 else if (number == 2)
                 {
-                    DisplayScores(scores, subject);
+
+
+                    DisplayScores(scores, subject, index);
+
+
                 }
                 else if (number == 3)
                 {
+
+
                     int totalScore = ScoreTotalCalculation(scores, index);
-                   
+
                     Console.WriteLine($"Total Sum: {totalScore}");
-                    
+
+
                 }
                 else if (number == 4)
                 {
+
+
                     int totalScore = ScoreTotalCalculation(scores, index);
-                    double averageScore = totalScore / number;
+                    double averageScore = (double) totalScore / index;
                     Console.WriteLine($"Average Score: {averageScore}");
-                    
+
+
                 }
                 else if (number == 5)
                 {
                     Console.WriteLine("Goodbye! ");
                     break;
+
+
                 }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid Input");
+            }
 
 
-        } while (number >= 1 && number <= 4);
+        } while (number >= 1 && number <= 5);
 
+        
     }
 
-
-    static void DisplayScores(int[] scores, string[] subject)
+    
+    //This method displays the subject name and score of the tests
+    static void DisplayScores(int[] scores, string[] subject, int index)
     {
-        for (int i = 0; i < scores.Length; i++)
+        /* A bug I was facing while working on my program is that since I used scores.Length in the DisplayScores() method,
+         the program kept getting empty values displayed. To fix it I passed down the index variable and did “i < index” 
+         so that ONLY the scores entered can be displayed on the program */
+        for (int i = 0; i < index; i++) 
         {
+            
             Console.WriteLine($"Subject: {subject[i]}, Score: {scores[i]}");
+            
         }
         
     }
 
+    //This method calculates the sum of the scores entered on the program
     static int ScoreTotalCalculation(int[] scores, int index)
     {
         int totalSum = 0;
