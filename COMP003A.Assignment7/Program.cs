4f1c3c2f57af705d79fcdd28a583d8f71841003d
briannaordaz <briannaordaz5@gmail.com>
2026-02-27 21:42:27 -0800

    declared and created a string and int array to store the test subjects and scores, created a do-while loop for the menu options

--- COMP003A.Assignment7/Program.cs
+++ COMP003A.Assignment7/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-
+using System.Collections.Generic;
 
 // Theme A - Student Score Analyzer
 //      Process a set of monetary values representing expenses.
@@ -9,13 +9,53 @@ class Program
 {
     static void Main()
     {
-        Console.Write("1. Enter Test Scores\n2. Display Scores\n3.\n4. Exit ");
-        
-        Console.Write("Enter number: ");
-        int number = int.Parse(Console.ReadLine());
-        
-        
-        
-        
+        string[] subject = new string[5];
+        int[] scores = new int[5];
+
+        Console.Write("1. Enter Test Scores\n2. Display Scores\n3. Analyze Scores \n4. Exit ");
+
+
+        int number;
+
+        do
+        {
+            Console.WriteLine("Enter a menu option (1-4): ");
+            number = int.Parse(Console.ReadLine());
+
+                
+                if (number < 1 || number > 4)
+                {
+                    Console.WriteLine("Invalid option. Please try again.");
+
+                }
+                else if (number == 1)
+                {
+                    Console.WriteLine("Enter Test Subject: ");
+                    string subjectName = Console.ReadLine();
+
+                    Console.WriteLine("Enter Test Score: ");
+                    double score = double.Parse(Console.ReadLine());
+
+                }
+                else if (number == 2)
+                {
+                    
+                }
+                else if (number == 3)
+                {
+                    
+                }
+                else if (number == 4)
+                {
+                    Console.WriteLine("Goodbye! ");
+                    break;
+                }
+
+
+        } while (number >= 1 && number <= 4);
+
     }
+
+    
+    
 }
