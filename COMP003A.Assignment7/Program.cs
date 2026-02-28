07e6919e822d1c84bcfa7afcb57a5a521cc30004
briannaordaz <briannaordaz5@gmail.com>
2026-02-27 21:00:30 -0800

    added menu prompt

---
+++ COMP003A.Assignment7/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+// Theme A - Student Score Analyzer
+//      Process a set of monetary values representing expenses.
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("1. Enter Test Scores\n2. Display Scores\n3.\n4. Exit ");
+        
+        Console.Write("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+        
+        
+        
+        
+    }
+}
